81cf422f698cd4c9ac9f3a567d3228e401f31654
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2016-03-12 10:44:08 -0700

    Changed Rotation implementation.

--- source/Rotation.hpp
+++ source/Rotation.hpp
@@ -1,6 +1,8 @@
 #ifndef ROTATION_HPP
 #define ROTATION_HPP
 
+#include <cmath>
+
 template<typename T>
 constexpr T pi() { return 3.1415926535897932384626433832795028841971; }
 
@@ -21,49 +23,43 @@ class Rotation
 {
     T _radians;
 
-    constexpr Rotation(T radians) : _radians(radians) {}
-
-    static T cycle(T radians)
-    {
-        if (radians >= pi<T>())
-            radians -= tau<T>();
-        else if (radians < -pi<T>())
-            radians += tau<T>();
-
-        return radians;
-    }
+    Rotation(T radians) : _radians(radians) {}
 
 public:
-    static constexpr Rotation fromDegrees(T degrees)
+    static Rotation fromRadians(T radians)
     {
-        return Rotation(degrees * radiansPerDegree<T>());
+        radians = std::fmod(radians, tau<T>());
+        if (radians < 0.0f) radians += tau<T>();
+        return Rotation(radians);
     }
 
-    static constexpr Rotation fromRadians(T radians)
+    static Rotation fromDegrees(T degrees)
     {
-        return Rotation(radians);
+        return fromRadians(degrees * radiansPerDegree<T>());
     }
 
     static constexpr const Rotation half()
     {
-        return Rotation(-pi<T>());
+        return Rotation(pi<T>());
     }
 
     constexpr Rotation() : _radians(0) {}
     constexpr Rotation(const Rotation&) = default;
     ~Rotation() = default;
 
-    Rotation<T>& operator=(const Rotation<T>& other) = default;
+    Rotation<T>& operator=(const Rotation<T>&) = default;
 
     Rotation<T>& operator+=(const Rotation<T>& other)
     {
-        _radians = cycle(_radians + other._radians);
+        _radians += other._radians;
+        if (_radians >= tau<T>()) _radians -= tau<T>();
         return *this;
     }
 
     Rotation<T>& operator-=(const Rotation<T>& other)
     {
-        _radians = cycle(_radians - other._radians);
+        _radians -= other._radians;
+        if (_radians < 0.0f) _radians += tau<T>();
         return *this;
     }
 
@@ -77,8 +73,8 @@ public:
         return Rotation<T>(_radians) -= other;
     }
 
-    constexpr T toRadians() const { return _radians; }
-    constexpr T toDegrees() const { return _radians * degreesPerRadian<T>(); }
+    T toRadians() const { return _radians; }
+    T toDegrees() const { return _radians * degreesPerRadian<T>(); }
 };
 
 typedef Rotation<float> RotationF;
